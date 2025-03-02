a96fbb9fe8aad11c697e843338b4c01bd2ad51f7
Ruan Ribeiro <ruan.pablo.pec@hotmail.com>
2025-03-02 14:40:57 -0300

    test

--- auth-service/service/healthcheck/routes.go
+++ auth-service/service/healthcheck/routes.go
@@ -1,29 +1,28 @@
-package healthcheck
+// package healthcheck
 
-import (
-	"net/http"
+// import (
+// 	"net/http"
 
-	// "github.com/hoyci/ms-chat/auth-service/types"
-	// "github.com/hoyci/ms-chat/auth-service/utils"
-	"github.com/hoyci/ms-chat/auth-service/types"
-	"github.com/hoyci/ms-chat/ms-core/utils"
-)
+// 	"github.com/hoyci/ms-chat/auth-service/types"
+// 	// "github.com/hoyci/ms-chat/auth-service/types"
+// 	// "github.com/hoyci/ms-chat/auth-service/utils"
+// )
 
-type HealthCheckHandler struct {
-	cfg types.Config
-}
+// type HealthCheckHandler struct {
+// 	cfg types.Config
+// }
 
-func NewHealthCheckHandler(cfg types.Config) *HealthCheckHandler {
-	return &HealthCheckHandler{
-		cfg: cfg,
-	}
-}
+// func NewHealthCheckHandler(cfg types.Config) *HealthCheckHandler {
+// 	return &HealthCheckHandler{
+// 		cfg: cfg,
+// 	}
+// }
 
-func (h *HealthCheckHandler) HandleHealthCheck(w http.ResponseWriter, r *http.Request) {
-	utils.WriteJSON(w, http.StatusOK, &types.HealthcheckResponse{
-		Status: "available",
-		SystemInfo: map[string]string{
-			"environment": h.cfg.Environment,
-		},
-	})
-}
+// func (h *HealthCheckHandler) HandleHealthCheck(w http.ResponseWriter, r *http.Request) {
+// 	utils.WriteJSON(w, http.StatusOK, &types.HealthcheckResponse{
+// 		Status: "available",
+// 		SystemInfo: map[string]string{
+// 			"environment": h.cfg.Environment,
+// 		},
+// 	})
+// }
