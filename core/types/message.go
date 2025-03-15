3822b843551450db94dc6d3e9af6c6522300dcf7
Ruan Pablo <65083150+Hoyci@users.noreply.github.com>
2025-03-15 15:13:00 -0300

    change type on core lib

--- core/types/message.go
+++ core/types/message.go
@@ -1,11 +1,12 @@
 package types
 
 type Message struct {
-	TempID    string `json:"temp_id" validate:"required"`
-	Type      string `json:"type" validate:"required"`
-	UserID    int    `json:"user_id" validate:"required"`
-	RoomID    string `json:"room_id" validate:"required"`
-	Content   string `json:"content" validate:"required"`
-	Timestamp string `json:"timestamp"`
-	ClientID  string `json:"client_id"`
+	TempID     string `json:"temp_id" validate:"required"`
+	Type       string `json:"type" validate:"required"`
+	SenderID   int    `json:"sender_id" validate:"required"`
+	ReceiverID int    `json:"receiver_id" validate:"required"`
+	RoomID     string `json:"room_id" validate:"required"`
+	Content    string `json:"content" validate:"required"`
+	Timestamp  string `json:"timestamp"`
+	ClientID   string `json:"client_id"`
 }
