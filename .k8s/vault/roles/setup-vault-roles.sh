d1198b952c7c9bb25fa9d62b23b751bca87ff636
Ruan Pablo <65083150+Hoyci@users.noreply.github.com>
2025-04-11 14:54:13 -0300

    fix script and workflow

--- .k8s/vault/roles/setup-vault-roles.sh
+++ .k8s/vault/roles/setup-vault-roles.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+export VAULT_ADDR="http://vault.vault.svc:8200"
+export VAULT_TOKEN="$VAULT_ROOT_TOKEN"
+
 MICROSERVICES=("auth-service")
 
 for SERVICE in "${MICROSERVICES[@]}"; do
-  kubectl exec -n vault vault-0 -- vault policy write "${SERVICE}-policy" - <<EOF
+  vault policy write "${SERVICE}-policy" - <<EOF
 path "secret/data/${SERVICE}/*" {
   capabilities = ["read", "list"]
 }
 EOF
 
-  kubectl exec -n vault vault-0 -- vault write auth/kubernetes/role/"${SERVICE}-role" \
+  vault write auth/kubernetes/role/"${SERVICE}-role" \
     bound_service_account_names="${SERVICE}-sa" \
     bound_service_account_namespaces="default" \
     policies="${SERVICE}-policy" \
